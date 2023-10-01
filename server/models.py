7fc77bf262c91859a6aa796c08d41706e823e214
Aadil Ahmed <aadilahmed0@gmail.com>
2023-10-01 03:28:04 -0700

    add requirements.txt file

--- server/models.py
+++ server/models.py
@@ -1,8 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
-
-
+from config import db, bcrypt
 
     
