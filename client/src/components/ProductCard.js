d6927e62eca61775330ea625e81af6ae618bf061
Aadil Ahmed <aadilahmed0@gmail.com>
2023-10-07 03:48:28 -0700

    create ProductCollection.js

---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProductCard() {
+    return (
+        <h1>Product Card</h1>
+    )
+}
+
+export default ProductCard
--- client/src/components/ProductCollection.js
+++ client/src/components/ProductCollection.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import ProductCard from "./ProductCard";
+import { Card } from "semantic-ui-react";
 
-function ProductCollection() {
-    return (
-        <h1>Product Collection</h1>
-    )
+function ProductCollection({ products }) {
+  const productsToDisplay = products.map((product) => (
+    <ProductCard
+      key={product.id}
+      name={product.name}
+      url={product.url}
+      current_price={product.current_price}
+    />
+  ));
+
+  return <Card.Group itemsPerRow={4}>{productsToDisplay}</Card.Group>;
 }
 
-export default ProductCollection
+export default ProductCollection;
