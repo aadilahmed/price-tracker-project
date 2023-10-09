a8fa822d91030926da94ebdbd3e2d6d08ae64b9e
Aadil Ahmed <aadilahmed0@gmail.com>
2023-10-08 20:21:05 -0700

    implement WishlistForm component

--- client/src/components/WishlistForm.js
+++ client/src/components/WishlistForm.js
@@ -1,8 +1,58 @@
-import React from "react"
+import React, {useState} from "react"
+import { Form, Grid, Segment } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
 
 function WishlistForm() {
+    const [title, setTitle] = useState("")
+    
+    const history = useHistory()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/wishlists", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+          }),
+        }).then((response) => {
+            response.json().then((user) => onLogin(user));
+        });
+    
+        history.push("/wishlists");
+      }
+
     return (
-        <h1>Wishlist Form!</h1>
+        <Grid
+        textAlign="center"
+        style={{ height: "100vh", margin: "0"}}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Segment stacked>
+            <h3>Create a Wishlist!</h3>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group widths="equal" grouped>
+                <Form.Input
+                  fluid
+                  label="Wishlist Title"
+                  placeholder="Enter Title"
+                  name="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Button
+                style={{ background: "#dc3545", color: "aliceblue" }}
+              >
+                Submit
+              </Form.Button>
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     )
 }
 
