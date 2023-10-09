85ad6a0f585620c530946c40f1e1f2c78df9d731
Aadil Ahmed <aadilahmed0@gmail.com>
2023-10-09 05:24:11 -0700

    remove dropdown code from ProductDetail

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -13,21 +13,10 @@ function ProductDetail() {
       .then((data) => setProduct(data));
   }, [id]);
 
-  //const dropdownItems = wishlists.map((wishlist) =>  <Dropdown.Item text={wishlist.title} />);
-
   return (
     <div className="productdetailpage-container">
       <h1>{product.name}</h1>
       <h2>$ {product.current_price / Math.pow(10, 2)}</h2>
-      <Dropdown text="File">
-        <Dropdown.Menu>
-          <Dropdown.Item text="New" />
-          <Dropdown.Item text="Open..." description="ctrl + o" />
-          <Dropdown.Item icon="trash" text="Move to trash" />
-          <Dropdown.Divider />
-          <Dropdown.Item text="E-mail Collaborators" />
-        </Dropdown.Menu>
-      </Dropdown>
       <Image className="ui large image" src={product.image} alt="" />
       {product.prices ? <PricesGraph prices={product.prices} /> : null}
     </div>
